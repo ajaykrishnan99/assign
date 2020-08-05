6652ec9782b5ace3052320e0db7de05b200fa140
ajaykrishnan99 <ajaykrishnan@gmail.com>
2020-08-06 01:49:10 +0530

    last changes"

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -16,7 +16,12 @@ export class AppComponent implements OnInit {
   i1;
   s;
   result=[];
+
+  // on Enter button  to add to todo list
   enter(event:any){
+
+    // Keycode for enter key is 13 so if code is 13 we add the list
+
     if (event.keyCode==13){
       if(this.ip.length!=1){
     
@@ -29,6 +34,8 @@ export class AppComponent implements OnInit {
     }
 
    }
+
+  //  on button click to add to todo list
   add() 
    {
      if(this.ip.length!=1){
@@ -38,6 +45,7 @@ export class AppComponent implements OnInit {
    }
    else{alert("empty");}
   }
+  // To remove the tdoo
    remove(i1:any)
    {
     this.s=this.result.indexOf(i1);
@@ -45,19 +53,21 @@ export class AppComponent implements OnInit {
    }
   
    btn(){
-   
     
     if(this.typed.length==1){
     alert("EMPTY");}
     else{alert(this.typed);}
   
    }
-   
+
+  //  Form Validation
    ngOnInit(){
     this.signupForm=new FormGroup({
       'username':new FormControl(null,Validators.required),'email': new FormControl(null,[Validators.required,Validators.email])
     });
    }
+
+  //  Form submit function
    onsubmit(){
      console.log(this.signupForm);
      if(this.signupForm.valid==true){alert("Form Submitted");}
